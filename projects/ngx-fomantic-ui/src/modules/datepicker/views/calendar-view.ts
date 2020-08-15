80d5a1deda30fc824be96833c24c6413aa6463a0
TryHardDood <rsnconfigs@gmail.com>
2020-08-15 16:38:05 +0200

    Calendarview Fix

--- projects/ngx-fomantic-ui/src/modules/datepicker/views/calendar-view.ts
+++ projects/ngx-fomantic-ui/src/modules/datepicker/views/calendar-view.ts
@@ -1,192 +1,192 @@
-import { AfterViewInit, Input, OnDestroy, QueryList, Renderer2, ViewChildren, Directive } from '@angular/core';
-import {KeyCode} from '../../../misc/util/internal';
-import {CalendarItem, FuiCalendarItem} from '../directives/calendar-item';
-import {CalendarService} from '../services/calendar.service';
-import {CalendarRangeService} from '../services/calendar-range.service';
+import { Input, QueryList, ViewChildren, AfterViewInit, HostListener, Renderer2, OnDestroy, Directive } from "@angular/core";
+import { KeyCode } from "../../../misc/util/internal";
+import { CalendarItem, FuiCalendarItem } from "../directives/calendar-item";
+import { CalendarService } from "../services/calendar.service";
+import { CalendarRangeService } from "../services/calendar-range.service";
 
 export enum CalendarViewType {
-  Year = 0,
-  Month = 1,
-  Date = 2,
-  Hour = 3,
-  Minute = 4
+    Year = 0,
+    Month = 1,
+    Date = 2,
+    Hour = 3,
+    Minute = 4
 }
-
 export type CalendarViewResult = [Date, CalendarViewType];
 
 @Directive()
 export abstract class CalendarView implements AfterViewInit, OnDestroy {
+    private _type:CalendarViewType;
+    private _service:CalendarService;
 
-  public ranges: CalendarRangeService;
-  private _type: CalendarViewType;
-  @ViewChildren(FuiCalendarItem)
-  private _renderedItems: QueryList<FuiCalendarItem>;
-  private _highlightedItem?: CalendarItem;
-  private _documentKeyDownListener: () => void;
+    @ViewChildren(FuiCalendarItem)
+    private _renderedItems:QueryList<FuiCalendarItem>;
+    private _highlightedItem?:CalendarItem;
 
-  constructor(renderer: Renderer2, viewType: CalendarViewType, ranges: CalendarRangeService) {
-    this._type = viewType;
-    this.ranges = ranges;
+    @Input()
+    public set service(service:CalendarService) {
+        this._service = service;
+        this.ranges.loadService(service);
 
-    this._documentKeyDownListener = renderer.listen('document', 'keydown', (e: KeyboardEvent) => this.onDocumentKeyDown(e));
-  }
+        this.service.onManualUpdate = () => {
+            this.ranges.refresh();
 
-  public get currentDate(): Date {
-    return this.service.currentDate;
-  }
+            delete this._highlightedItem;
+            this.autoHighlight();
+        };
+    }
 
-  public get selectedDate(): Date | undefined {
-    return this.service.selectedDate;
-  }
+    public get service():CalendarService {
+        return this._service;
+    }
 
-  private _service: CalendarService;
+    public ranges:CalendarRangeService;
 
-  public get service(): CalendarService {
-    return this._service;
-  }
+    public get currentDate():Date {
+        return this.service.currentDate;
+    }
 
-  @Input()
-  public set service(service: CalendarService) {
-    this._service = service;
-    this.ranges.loadService(service);
+    public get selectedDate():Date | undefined {
+        return this.service.selectedDate;
+    }
 
-    this.service.onManualUpdate = () => {
-      this.ranges.refresh();
+    private _documentKeyDownListener:() => void;
 
-      delete this._highlightedItem;
-      this.autoHighlight();
-    };
-  }
+    constructor(renderer:Renderer2, viewType:CalendarViewType, ranges:CalendarRangeService) {
+        this._type = viewType;
+        this.ranges = ranges;
 
-  // Template Methods
+        this._documentKeyDownListener = renderer.listen("document", "keydown", (e:KeyboardEvent) => this.onDocumentKeyDown(e));
+    }
 
-  public setDate(item: CalendarItem): void {
-    this.service.changeDate(item.date, this._type);
-  }
+    // Template Methods
 
-  public zoomOut(): void {
-    this.service.zoomOut(this._type);
-  }
+    public setDate(item:CalendarItem):void {
+        this.service.changeDate(item.date, this._type);
+    }
 
-  // Keyboard Control
+    public zoomOut():void {
+        this.service.zoomOut(this._type);
+    }
 
-  public ngAfterViewInit(): void {
-    this._renderedItems.changes.subscribe(() => this.onRenderedItemsChanged());
-    this.onRenderedItemsChanged();
-  }
+    // Keyboard Control
 
-  public ngOnDestroy(): void {
-    this._documentKeyDownListener();
-  }
+    public ngAfterViewInit():void {
+        this._renderedItems.changes.subscribe(() => this.onRenderedItemsChanged());
+        this.onRenderedItemsChanged();
+    }
 
-  private onRenderedItemsChanged(): void {
-    this._renderedItems.forEach(i =>
-      i.onFocussed.subscribe((hasFocus: boolean) => {
-        if (hasFocus) {
-          this.highlightItem(i.item);
+    private onRenderedItemsChanged():void {
+        this._renderedItems.forEach(i =>
+            i.onFocussed.subscribe((hasFocus:boolean) => {
+                if (hasFocus) {
+                    this.highlightItem(i.item);
+                }
+            }));
+
+        this.autoHighlight();
+        this.highlightItem(this._highlightedItem);
+    }
+
+    private autoHighlight():void {
+        let date = this.selectedDate && this.ranges.current.containsDate(this.selectedDate) ? this.selectedDate : this.currentDate;
+        if (this._highlightedItem && this.ranges.current.containsDate(this._highlightedItem.date)) {
+            date = this._highlightedItem.date;
         }
-      }));
 
-    this.autoHighlight();
-    this.highlightItem(this._highlightedItem);
-  }
-
-  private autoHighlight(): void {
-    let date = this.selectedDate && this.ranges.current.containsDate(this.selectedDate) ? this.selectedDate : this.currentDate;
-    if (this._highlightedItem && this.ranges.current.containsDate(this._highlightedItem.date)) {
-      date = this._highlightedItem.date;
+        const initiallyHighlighted = this.ranges.current.items.find(i => this.ranges.dateComparer.equal(i.date, date));
+        if (initiallyHighlighted && !initiallyHighlighted.isDisabled) {
+            this._highlightedItem = initiallyHighlighted;
+        }
     }
 
-    const initiallyHighlighted = this.ranges.current.items.find(i => this.ranges.dateComparer.equal(i.date, date));
-    if (initiallyHighlighted && !initiallyHighlighted.isDisabled) {
-      this._highlightedItem = initiallyHighlighted;
-    }
-  }
+    private highlightItem(item:CalendarItem | undefined):void {
+        if (item) {
+            this._renderedItems.forEach(i => i.hasFocus = false);
+            const rendered = this._renderedItems.find(ri => ri.item === item);
+            if (rendered && !rendered.hasFocus) {
+                rendered.hasFocus = true;
+                rendered.changeDetector.detectChanges();
+            }
 
-  private highlightItem(item: CalendarItem | undefined): void {
-    if (item) {
-      this._renderedItems.forEach(i => i.hasFocus = false);
-      const rendered = this._renderedItems.find(ri => ri.item === item);
-      if (rendered && !rendered.hasFocus) {
-        rendered.hasFocus = true;
-        rendered.changeDetector.detectChanges();
-      }
-
-      this._highlightedItem = item;
-    }
-  }
-
-  private onDocumentKeyDown(e: KeyboardEvent): void {
-    if (this._highlightedItem && e.keyCode === KeyCode.Enter) {
-      this.setDate(this._highlightedItem);
-      return;
+            this._highlightedItem = item;
+        }
     }
 
-    if (!this._highlightedItem) {
-      this.autoHighlight();
+    private onDocumentKeyDown(e:KeyboardEvent):void {
+        if (this._highlightedItem && e.keyCode === KeyCode.Enter) {
+            this.setDate(this._highlightedItem);
+            return;
+        }
+
+        if (!this._highlightedItem) {
+            this.autoHighlight();
+        }
+
+        const index = this.ranges.current.findIndex(this._highlightedItem);
+        let isMovingForward = true;
+        let delta = 0;
+
+        switch (e.keyCode) {
+            case KeyCode.Right:
+                delta += 1;
+                break;
+            case KeyCode.Left:
+                delta -= 1;
+                isMovingForward = false;
+                break;
+            case KeyCode.Down:
+                delta += this.ranges.columns;
+                break;
+            case KeyCode.Up:
+                delta -= this.ranges.columns;
+                isMovingForward = false;
+                break;
+            default:
+                return;
+        }
+
+        // Stop these keypresses being captured elsewhere.
+        e.preventDefault();
+
+        let nextItem = this.ranges.current.items[index + delta];
+
+        if (nextItem && nextItem.isDisabled) {
+            return;
+        }
+
+        if (nextItem && !nextItem.isOutsideRange) {
+            return this.highlightItem(nextItem);
+        }
+
+        if (nextItem && nextItem.isOutsideRange) {
+            if (index + delta >= this.ranges.current.inRange.length) {
+                isMovingForward = true;
+            }
+        }
+
+        if (!nextItem) {
+            let adjustedIndex = this.ranges.current.findIndex(this._highlightedItem);
+
+            const nextItems = this.ranges.calc(isMovingForward).inRange;
+
+            if (isMovingForward) {
+                adjustedIndex -= this.ranges.current.inRange.length;
+            } else {
+                adjustedIndex += nextItems.length;
+            }
+
+            nextItem = nextItems[adjustedIndex + delta];
+
+            if (nextItem.isDisabled) {
+                return;
+            }
+        }
+
+        this.ranges.move(isMovingForward);
+        this._highlightedItem = this.ranges.current.find(nextItem);
     }
 
-    const index = this.ranges.current.findIndex(this._highlightedItem);
-    let isMovingForward = true;
-    let delta = 0;
-
-    switch (e.keyCode) {
-      case KeyCode.Right:
-        delta += 1;
-        break;
-      case KeyCode.Left:
-        delta -= 1;
-        isMovingForward = false;
-        break;
-      case KeyCode.Down:
-        delta += this.ranges.columns;
-        break;
-      case KeyCode.Up:
-        delta -= this.ranges.columns;
-        isMovingForward = false;
-        break;
-      default:
-        return;
+    public ngOnDestroy():void {
+        this._documentKeyDownListener();
     }
-
-    // Stop these keypresses being captured elsewhere.
-    e.preventDefault();
-
-    let nextItem = this.ranges.current.items[index + delta];
-
-    if (nextItem && nextItem.isDisabled) {
-      return;
-    }
-
-    if (nextItem && !nextItem.isOutsideRange) {
-      return this.highlightItem(nextItem);
-    }
-
-    if (nextItem && nextItem.isOutsideRange) {
-      if (index + delta >= this.ranges.current.inRange.length) {
-        isMovingForward = true;
-      }
-    }
-
-    if (!nextItem) {
-      let adjustedIndex = this.ranges.current.findIndex(this._highlightedItem);
-
-      const nextItems = this.ranges.calc(isMovingForward).inRange;
-
-      if (isMovingForward) {
-        adjustedIndex -= this.ranges.current.inRange.length;
-      } else {
-        adjustedIndex += nextItems.length;
-      }
-
-      nextItem = nextItems[adjustedIndex + delta];
-
-      if (nextItem.isDisabled) {
-        return;
-      }
-    }
-
-    this.ranges.move(isMovingForward);
-    this._highlightedItem = this.ranges.current.find(nextItem);
-  }
 }
