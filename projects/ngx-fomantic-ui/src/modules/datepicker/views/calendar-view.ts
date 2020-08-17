c745bb7da79e64ca996e2a001218fc33c178e6ec
TryHardDood <10364896+TryHardDood@users.noreply.github.com>
2020-08-17 12:35:52 +0200

    Angular 10 update

--- projects/ngx-fomantic-ui/src/modules/datepicker/views/calendar-view.ts
+++ projects/ngx-fomantic-ui/src/modules/datepicker/views/calendar-view.ts
@@ -1,4 +1,4 @@
-import { Input, QueryList, ViewChildren, AfterViewInit, HostListener, Renderer2, OnDestroy, Directive } from "@angular/core";
+import { Input, QueryList, ViewChildren, AfterViewInit, Renderer2, OnDestroy } from "@angular/core";
 import { KeyCode } from "../../../misc/util/internal";
 import { CalendarItem, FuiCalendarItem } from "../directives/calendar-item";
 import { CalendarService } from "../services/calendar.service";
@@ -13,7 +13,6 @@ export enum CalendarViewType {
 }
 export type CalendarViewResult = [Date, CalendarViewType];
 
-@Directive()
 export abstract class CalendarView implements AfterViewInit, OnDestroy {
     private _type:CalendarViewType;
     private _service:CalendarService;
